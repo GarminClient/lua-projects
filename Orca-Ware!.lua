e679f9ed0219627b2152e1f6d93e765d01579846
MoonYes <98134735+GarminClient@users.noreply.github.com>
2022-06-09 18:30:48 -0300

    Update Orca-Ware!.lua

--- Orca-Ware!.lua
+++ Orca-Ware!.lua
@@ -1,32 +1,9 @@
-if not game.Players.LocalPlayer:IsInGroup(13578487) then
+--i know a lot of people will skid this but this is easy to get
 
-    coroutine.resume(coroutine.create(function()
+local args = {
+    [1] = "ReturningCommercialWords",
+    [2] = 1,
+    [4] = "orca-Ware on top"
+}
 
-       wait(10)
-
-       game.StarterGui:SetCore("SendNotification", {
-
-        Title = 'Gartic Loading process Error';
-
-        Text = 'to execute Gartic You need to join they group that was copied to your clipboard';
-
-        Duration = 20;
-
-       })
-
-       setclipboard('https://web.roblox.com/groups/13578487/CatWare#!/about')
-
-    end))
-
-  end
-
-end)
-
-if Player:IsInGroup("13578487") then
-game.StarterGui:SetCore("SendNotification",  {
- Title = "Gartic";
- Text = "Gartic Succesfully loaded";
- Duration = 60;
- Button1 = "ok";
- Callback = NotificationBindable;
-})
+game:GetService("ReplicatedStorage").Ree["28Cemeter28y"]:FireServer(unpack(args))
