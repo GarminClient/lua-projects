c751fb486671b41b0ff1bf3f4fb382be8bdcefce
MoonYes <98134735+GarminClient@users.noreply.github.com>
2022-06-09 18:36:24 -0300

    Update Orca-Ware!.lua

--- Orca-Ware!.lua
+++ Orca-Ware!.lua
@@ -7,3 +7,27 @@ local args = {
 }
 
 game:GetService("ReplicatedStorage").Ree["28Cemeter28y"]:FireServer(unpack(args))
+
+local args = {
+    [1] = "BusinessName",
+    [2] = "orca-ware on top"
+}
+
+game:GetService("ReplicatedStorage").Ree:FindFirstChild("28RPHous28eEven28t"):FireServer(unpack(args))
+
+local args = {
+    [1] = "ReturningCommercialWords",
+    [2] = 3,
+    [4] = "Orca-Ware Winning"
+}
+
+game:GetService("ReplicatedStorage").Ree:FindFirstChild("28Cemeter28y"):FireServer(unpack(args))
+
+local args = {
+    [1] = "ReturningCommercialWords",
+    [2] = 2,
+    [4] = "Orca-Ware Is On Top"
+}
+
+game:GetService("ReplicatedStorage").Ree:FindFirstChild("28Cemeter28y"):FireServer(unpack(args))
+
